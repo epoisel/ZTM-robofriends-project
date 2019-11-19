798625d7bd3c582452ee8361bbf4a513b8eed131
William Poisel <epoisel133@gmail.com>
2019-11-18 19:15:46 -0500

    Updates

---
+++ precache-manifest.6730bab8df430fd24ba69d5841a0354d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f971bd967ca582284af918dae79557d0",
+    "url": "/ZTM-robofriends-project/index.html"
+  },
+  {
+    "revision": "539a39d4a9d9d27e97e4",
+    "url": "/ZTM-robofriends-project/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "413648eb5b75eee1e5c5",
+    "url": "/ZTM-robofriends-project/static/css/main.9671dbbe.chunk.css"
+  },
+  {
+    "revision": "539a39d4a9d9d27e97e4",
+    "url": "/ZTM-robofriends-project/static/js/2.761caa3a.chunk.js"
+  },
+  {
+    "revision": "413648eb5b75eee1e5c5",
+    "url": "/ZTM-robofriends-project/static/js/main.a73c325e.chunk.js"
+  },
+  {
+    "revision": "aec7146a8971ca6c83df",
+    "url": "/ZTM-robofriends-project/static/js/runtime-main.b35bc052.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robofriends/precache-manifest.1bb9cad9f8c8e871aaa2988b357945f4.js"
+  "/ZTM-robofriends-project/precache-manifest.6730bab8df430fd24ba69d5841a0354d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robofriends/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ZTM-robofriends-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
